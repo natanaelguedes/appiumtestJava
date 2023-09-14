3ce68f34414ff2d851e81ca05f046431487aba67
natanaelguedes <ngsneto@gmail.com>
2023-09-14 18:07:08 -0300

    add
    add

--- TestAppiumJava2/src/main/java/PageTest/FormularioTeste.java
+++ TestAppiumJava2/src/main/java/PageTest/FormularioTeste.java
@@ -65,28 +65,18 @@ public class FormularioTeste {
 
 		  @Test 
 		  public void RealizarCadastro() throws MalformedURLException{
-		  dsl.escrever(By.className("android.widget.EditText"),"Natanael");
+		   page.escreverNome("Natanael");
+		   page.clicarCheck();
+		   page.clicarSwitch();
+		   page.selecionarCombo("Nitendo Switch");
+           page.Salvar();
+
+		   dsl.escrever(By.className("android.widget.EditText"),"Natanael");
 		 dsl.clicar(By.className("android.widget.CheckBox"));//.click();
 		 dsl.clicar(By.className("android.widget.Switch"));//).cbarracolick();
 		 dsl.selecionarCombo(By.className("android.widget.Spinner"),"Nintendo Switch") ;//.click(); dsl.clicarPorTexto("SALVAR"); //;
 		 dsl.clicarPorTexto("SALVAR");//.click();
 		  
-		 //validar //driver.findElement(By.xpath("//*[@text='SALVAR']")).click();
-		 
-		 Assert.assertEquals("Nome: Natanael", dsl.obterTexto(By.xpath("//android.widget.TextView[@text='Nome: Natanael']")));
-		 Assert.assertEquals("Console: switch",dsl.obterTexto(By.xpath("//android.widget.TextView[@text='Console: switch']")));
-		 Assert.assertTrue(dsl.obterTexto(By.xpath("//android.widget.TextView[starts-with(@text,'Switch:')]")).endsWith("Off"));
-		 Assert.assertTrue(dsl.obterTexto(By.xpath("//android.widget.TextView[starts-with(@text,'Checkbox:')]")).endsWith("Marcado"));
- 
-//		 Assert.assertTrue(dsl.isCheckMarcado.(By.xpath("//android.widget.TextView[starts-with(@text,'Switch')]".endsWith(null))("android.widget.CheckBox").en) ).;
-	//		 Assert.assertFalse(dsl.isCheckMarcado(By.className("android.widget.CheckBox")));
-		 // Assert.assertTrue("0ff",dsl.obterTexto(By.xpath("//android.widget.TextView[start-with(@text,'Checkbox:'])")).endsWith("Marcado")); //start-with 
-		/*  MobileElement date = driver.findElement(By.xpath("//android.widget.TextView[@text='Data: 01/01/2000']"));
-		  Assert.assertEquals("Data: 01/01/2000",date.getText());
-		  MobileElement hora =dsl.obterTexto(By.xpath("//android.widget.TextView[@text='Hora: 06:00']"));
-		 Assert.assertEquals("Hora: 06:00",hora.getText());
-		*/ 	  //}
-		 // "//android.widget.TextView[start-with(@text,'Checkbox:'])"
-		  }
+			  }
 }	 
 
--- TestAppiumJava2/src/main/java/page/FormularioPage.java
+++ TestAppiumJava2/src/main/java/page/FormularioPage.java
@@ -18,14 +18,13 @@ public class FormularioPage {
  }
  
 //}
- public void ObterValorCombo(String v) {
-   dsl.selecionarCombo(MobileBy.AccessibilityId("console"),v); 
+ public void selecionarCombo(String v) {
+   dsl.selecionarCombo(MobileBy.AccessibilityId("console"),v);//.xpath("//android.widget.Spinner/android.widget.TextView")); 
  }
  
  public void clicarCheck() {
 	 dsl.clicar(By.className("android.widget.CheckBox"));
- 
-	 
+  
 	 
  }
  
@@ -40,5 +39,46 @@ public class FormularioPage {
 	return  dsl.isCheckMarcado(MobileBy.AccessibilityId("switch"));
  }
 
+ public void Salvar() {
+	 dsl.clicarPorTexto("SALVAR");
+ }
  
+ public String obterNomeCadastrado() {
+	 return dsl.obterTexto(By.xpath("//android.widget.TextView[starts-with(@text, 'Nome:'"));
+ }
+ public String obterConsoleCadastrado() {
+	 return dsl.obterTexto(By.xpath("//android.widget.TextView[starts-with(@text,'Console']"));
+ }
+ public String obterSwitchCadastrado() {
+	 //return  dsl.obterTexto(By.xpath("//android.widget.TextView[starts-with(@text='Checkbox']")).endsWith("Marcado"));
+	// return dsl.obterTexto(By.xpath("//android.widget.TextView[start-with(@text,'Checkbox:'])")).endsWith("Marcado")); //start-with 
+	return dsl.obterTexto(By.xpath("//android.widget.TextView[start-with(@text,'Switch:'])"));//.endsWith("Marcado")); //start-with 
+
+//	 return dsl.s "//android.widget.TextView[@text='Nome: Natanael'])))";
+ }
+ 
+ public String obterCheckboxCadastrado() {
+	 //return dsl.obterTexto(By.xpath("//android.widget.TextView[starts-with(@text,'Checkbox:'])")).endsWith("Marcado")); //.endsWith("Marcado")); //start-with 
+
+	 return dsl.obterTexto(By.xpath("//android.widget.TextView[start-with(@text,'Checkbox:'])"));//.endsWith("Marcado")); 
+ }
+ 
+ 
+ //validar //driver.findElement(By.xpath("//*[@text='SALVAR']")).click();
+ 
+ Assert.assertEquals("Nome: Natanael", dsl.obterTexto(By.xpath("//android.widget.TextView[@text='Nome: Natanael']")));
+ Assert.assertEquals("Console: switch",dsl.obterTexto(By.xpath("//android.widget.TextView[@text='Console: switch']")));
+ Assert.assertTrue(dsl.obterTexto(By.xpath("//android.widget.TextView[starts-with(@text,'Switch:')]")).endsWith("Off"));
+ Assert.assertTrue(dsl.obterTexto(By.xpath("//android.widget.TextView[starts-with(@text,'Checkbox:')]")).endsWith("Marcado"));
+
+// Assert.assertTrue(dsl.isCheckMarcado.(By.xpath("//android.widget.TextView[starts-with(@text,'Switch')]".endsWith(null))("android.widget.CheckBox").en) ).;
+//		 Assert.assertFalse(dsl.isCheckMarcado(By.className("android.widget.CheckBox")));
+ Assert.assertTrue("0ff",dsl.obterTexto(By.xpath("//android.widget.TextView[start-with(@text,'Checkbox:'])")).endsWith("Marcado")); //start-with 
+/*  MobileElement date = driver.findElement(By.xpath("//android.widget.TextView[@text='Data: 01/01/2000']"));
+  Assert.assertEquals("Data: 01/01/2000",date.getText());
+  MobileElement hora =dsl.obterTexto(By.xpath("//android.widget.TextView[@text='Hora: 06:00']"));
+ Assert.assertEquals("Hora: 06:00",hora.getText());
+*/ 	  //}
+ // "//android.widget.TextView[start-with(@text,'Checkbox:'])"
+
 }
